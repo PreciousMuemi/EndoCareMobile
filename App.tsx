4865de7c900490f6bbbeece37ccf158e171b9a96
H3nryK <henrykimani89@gmail.com>
2024-08-26 13:32:59 +0300

    bottom navabar

--- App.tsx
+++ App.tsx
@@ -3,7 +3,6 @@ import 'react-native-gesture-handler';
 import React  from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-import { createDrawerNavigator } from '@react-navigation/drawer';
 
 import WelcomeScreen from './src/screens/WelcomeScreen';
 import LoginScreen from './src/screens/LoginScreen';
@@ -11,7 +10,6 @@ import HomeScreen from './src/screens/HomeScreen';
 import SignupScreen from './src/screens/SignupScreen';
 
 const Stack = createStackNavigator();
-const Drawer = createDrawerNavigator();
 
 const App = () => {
     return (
@@ -23,9 +21,6 @@ const App = () => {
                 <Stack.Screen name="Signup" component={SignupScreen} />
                 <Stack.Screen name="Home" component={HomeScreen} options={{headerShown: false}} />
             </Stack.Navigator>
-            <Drawer.Navigator initialRouteName="Home">
-                <Drawer.Screen name="Home" component={HomeScreen} />
-            </Drawer.Navigator>
         </NavigationContainer>
         </>
     );
--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,15 +1,52 @@
 import * as React from 'react';
-import { Button, View } from 'react-native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import WelcomeScreen from './WelcomeScreen';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+
+const Tab = createBottomTabNavigator();
 
 function HomeScreen({ navigation }) {
-  return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Button
-        onPress={() => navigation.navigate('Notifications')}
-        title="Go to notifications"
-      />
-    </View>
-  );
+
+    return (
+        <Tab.Navigator
+          initialRouteName="Feed"
+          screenOptions={{
+            tabBarActiveTintColor: '#e91e63',
+          }}
+        >
+          <Tab.Screen
+            name="Feed"
+            component={WelcomeScreen}
+            options={{
+              tabBarLabel: 'Home',
+              tabBarIcon: ({ color, size }) => (
+                <MaterialCommunityIcons name="home" color={color} size={size} />
+              ),
+            }}
+          />
+          <Tab.Screen
+            name="Notifications"
+            component={WelcomeScreen}
+            options={{
+              tabBarLabel: 'Updates',
+              tabBarIcon: ({ color, size }) => (
+                <MaterialCommunityIcons name="bell" color={color} size={size} />
+              ),
+              tabBarBadge: 3,
+            }}
+          />
+          <Tab.Screen
+            name="Profile"
+            component={WelcomeScreen}
+            options={{
+              tabBarLabel: 'Profile',
+              tabBarIcon: ({ color, size }) => (
+                <MaterialCommunityIcons name="account" color={color} size={size} />
+              ),
+            }}
+          />
+        </Tab.Navigator>
+      );
 }
 
 export default HomeScreen;
